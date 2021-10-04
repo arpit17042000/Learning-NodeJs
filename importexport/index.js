f930d6566527418f9fecfcae1be5a617d5cb57d9
Arpit Prasad <arpitprasad92.8@gmail.com>
2021-10-04 19:13:04 +0530

    learnt how to import export

--- importexport/index.js
+++ importexport/index.js
@@ -1,2 +1,5 @@
-const add=require('./oper');
+const {add,sub,mult}=require('./oper');
 console.log(add(5,6));
+console.log(sub(5,6));
+console.log(mult(5,6));
+
--- importexport/oper.js
+++ importexport/oper.js
@@ -1,5 +1,11 @@
 const add=(a,b)=>{
     return a+b;
 }
+const sub=(a,b)=>{
+    return a-b;
+}
+const mult=(a,b)=>{
+    return a*b;
+}
 
-module.exports=add;
+module.exports={add,sub,mult};
