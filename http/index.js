f97175894191a8e441c054b1832b223f0f5c968a
Arpit Prasad <arpitprasad92.8@gmail.com>
2021-10-11 20:28:53 +0530

    learnt about routing in nodejs

--- http/index.js
+++ http/index.js
@@ -1,6 +1,13 @@
 const http=require('http');
 const server = http.createServer((req,res)=>{
-    res.end("hello there,how you doing")
+    if(req.url=='/Home'){
+        res.end("Home");
+    }else if(req.url=='/About'){
+        res.end("/About");
+    }else{
+        res.writeHead(404,{"Content-type":"text/html"});
+        res.end("<h1>404 error page not found</h1>")
+    }
 })
 
 server.listen(8000,"127.0.0.1",()=>{
