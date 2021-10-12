5aaa7fff235ad42691d4ff646ec219a664a3c50e
Arpit Prasad <arpitprasad92.8@gmail.com>
2021-10-12 19:18:26 +0530

    learnt how to create own API using node js

--- http/index.js
+++ http/index.js
@@ -1,15 +1,20 @@
-const http=require('http');
-const server = http.createServer((req,res)=>{
-    if(req.url=='/Home'){
-        res.end("Home");
-    }else if(req.url=='/About'){
-        res.end("/About");
-    }else{
-        res.writeHead(404,{"Content-type":"text/html"});
-        res.end("<h1>404 error page not found</h1>")
-    }
-})
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  const data = fs.readFileSync("./userAPI/userAPI.json", "utf8");
+  if (req.url == "/") {
+    res.end("Home");
+  } else if (req.url == "/About") {
+    res.end("/About");
+  } else if (req.url == "/API") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, { "Content-type": "text/html" });
+    res.end("<h1>404 error page not found</h1>");
+  }
+});
 
-server.listen(8000,"127.0.0.1",()=>{
-    console.log("listening to port 8000");
-})
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listening to port 8000");
+});
