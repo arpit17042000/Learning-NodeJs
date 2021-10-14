2ce67c52994cff9cbe0503066585bd7b78aec5a2
Arpit Prasad <arpitprasad92.8@gmail.com>
2021-10-14 19:40:45 +0530

    learnt about streams

---
+++ streamNodejs/index.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const http = require("http");
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+//   fs.readFile("readFile.txt", (err, data) => {
+//     if (err) console.log(err);
+//     res.end(data);
+//   });
+    const rstream=fs.createReadStream("readFile.txt");
+    rstream.on("data",(chunk)=>{
+        res.write(chunk);
+    })
+
+    rstream.on("end",()=>{
+        res.end();
+    })
+
+    rstream.on("error",(err)=>{
+        console.log(err);
+    })
+
+});
+
+server.listen(8000, "127.0.0.1");
